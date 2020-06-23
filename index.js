c63a73a6fc1d8ead27a34ba28c271b71ac19f712
zouantchaw <44246692+zouantchaw@users.noreply.github.com>
2020-06-23 15:28:46 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+  let writeCards = function(names, event_name) {
+      let thankYouMessages = []
+      for (let i = 0; i < names.length; i++) {
+        thankYouMessages.push(`Thank you, ${names[i]}, for the wonderful ${event_name} gift!`)
+          
+      }
+      return thankYouMessages
+  }
+
+  let countDown = function(number) {
+    let ticker = number  
+    while (ticker >= 0) {
+          console.log(ticker);
+          ticker--;
+      }
+  }
